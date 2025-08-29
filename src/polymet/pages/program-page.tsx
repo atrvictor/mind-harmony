1b5fecb2fb0877d5742c9bf879330f18ba7d9816
atrvictor <34503789+atrvictor@users.noreply.github.com>
2025-08-29 12:06:12 -0700

    Remove redundant 'Our Program' header from program page
    - Image already contains 'Program' title
    - Cleaner, more minimal design
    - Let the image speak for itself

--- src/polymet/pages/program-page.tsx
+++ src/polymet/pages/program-page.tsx
@@ -1,15 +1,6 @@
 export default function ProgramPage() {
   return (
     <div>
-      {/* Simple header */}
-      <div className="py-8 px-4 md:px-6 lg:px-8 bg-[#F5F0E5]/20">
-        <div className="max-w-4xl mx-auto text-center">
-          <h1 className="text-3xl md:text-4xl font-bold mb-4 text-[#1E3A5F]">
-            Our Program
-          </h1>
-        </div>
-      </div>
-
       {/* Program Image Section */}
       <section className="py-8 px-4 md:px-6 lg:px-8">
         <div className="max-w-4xl mx-auto">
